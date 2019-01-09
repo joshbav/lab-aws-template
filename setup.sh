16faac2332d49b5a6836462878be16a728c96b0d
“joshbav” <spam@nowhere.com>
2019-01-09 15:12:08 -0800

    scripted commit 01-09-19

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+echo
+echo YUM INSTALL OF ALL BUT DOCKER
+echo
+sudo yum install -y bind-utils which gawk curl gettext host iproute util-linux sed autofs nano ftp jq wget expect net-tools traceroute yum-utils device-mapper-persistent-data lvm2 which tar xz unzip curl ipset 
+
+echo
+echo ADD DOCKER REPO
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+echo
+echo INSTALL DOCKER
+sudo systemctl enable docker && sudo systemctl start docker && sudo usermod -aG docker centos && sudo docker run hello-world
+
+
+
