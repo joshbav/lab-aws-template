9365a3ab0ac485b24247015462d8aaf91fa5d9c3
“joshbav” <spam@nowhere.com>
2019-01-10 15:36:11 -0800

    scripted commit 10-Jan-2019-03:36:11

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+# This is just to update git
 git config user.name “joshbav” 
-git add *
+git add -A
 git commit -m "scripted commit $(date +%d-%b-%Y-%I:%M:%S)"
 git push -u origin master
