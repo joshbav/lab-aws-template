2d0e16e07c0bb5fff2de7e5d43a23526efe4eff9
“joshbav” <spam@nowhere.com>
2019-01-28 20:58:34 -0800

    Scripted commit 28-Jan-2019-08:58:34PM

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,36 +1,75 @@
 #! /bin/bash
+
+echo
+echo DELETING /ETC/MACHINE-ID, ADDING SYSTEMD UNIT TO REGENERATE IT ON REBOOT
+echo
+# Since this will be a base image, we'll delete this file so it'll be recreated
+#  when the first reboot happens, which should be in an instance booting from 
+#  a copy of this image
+# Not going to delete it now since we will reboot to install kernel headers
+# then delete the file then, then take a snapshot  # sudo rm -f /etc/machine-id
+sudo cp generate-machine-id.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable generate-machine-id.service
+echo
+
+echo
+echo UINSTALLING POSTFIX
+echo
+# Why is it even installed by default in CentOS?
+sudo yum remove -y postfix
+echo
+
+#### EXCLUDED FROM THIS SCRIPT
+## /etc/security/limits.conf
+## /etc/sysctl.conf
+####
+
 echo
 echo SETTING SOME ENV VARIABLES FOR ALL USERS
+echo
 sudo bash -c "echo \"LANG=en_US.UTF-8\" >> /etc/environment"
+#ENV LANG en_US.UTF-8
 sudo bash -c "echo \"LANGUAGE=en_US:en\" >> /etc/environment"
+#ENV LANGUAGE en_US:en
 sudo bash -c "echo \"LC_ALL=en_US.UTF-8\" >> /etc/environment"
-
-echo
-echo IF NTPD IS INSTALLED, REMOVING IT. INSTALLING CHRONY AND A CONFIG FILE
-echo
-# Will be using chrony for NTP, ntp package may not even be installed
-# Default NTP in AWS works fine, but this is useful for other environments
-sudo yum remove -y ntp
-sudo yum install -y chrony
-# Install chrony config file
-sudo cp chrony.conf /etc
-# No need to restart chrony since a reboot will be done
-
-echo
-echo YUM INSTALL OF MY MAIN PACKAGES
-echo
-sudo yum install -y epel-release yum-utils deltarpm
-sudo yum update -y
-# Much of this is in a base CentOS & RHEL install, but not necessarily in a container,
-#  although I wouldn't necessarily install all of it in a container.
-sudo yum install -y ansible autofs bash-completion binutils bind-utils bzip2 ca-certificates centos-release coreutils cpio curl device-mapper-persistent-data diffutils ethtool expect findutils ftp gawk grep gettext git gzip hardlink hostname iftop info iproute ipset iputils jq kubernetes-cli less lua lvm2 make man nano net-tools nfs-utils nload nmap openssh-clients passwd procps-ng rsync sed sudo sysstat tar tcping traceroute unzip util-linux vim wget which xz     
+#ENV ENV LC_ALL en_US.UTF-8
+sudo bash -c "echo \"TERM=xterm-256color\" >> /etc/environment"
+ENV TERM xterm-256color
 echo
 
 echo
 echo ADDING ALIASES TO /ETC/BASHRC
 echo
 # These are just my personal preferences
-sudo bash -c "echo \"alias s='sudo systemctl' j='journalctl' k='kubectl'\" >>/etc/bashrc"
+sudo bash -c "echo \"alias s='sudo systemctl' j='journalctl' k='kubectl' kdump='kubectl get all --all-namespaces'\" >>/etc/bashrc"
+# Note kdump won't show config maps, secrets, and ingress objects TODO: how to add?
+echo
+# Note K8s kubectl 1.14 uses -A for all name spaces
+
+echo
+echo YUM INSTALL OF MY MAIN PACKAGES
+echo
+sudo yum install -y epel-release yum-utils deltarpm
+sudo yum makecache -y
+# sudo yum update --security -y
+sudo yum update -y
+# Much of this is in a base CentOS & RHEL install, but not necessarily in a container,
+#  although I wouldn't necessarily install all of it in a container.
+sudo yum install -y ansible autofs bash-completion binutils bind-utils bzip2 ca-certificates centos-release coreutils cpio curl device-mapper-persistent-data diffutils ethtool expect findutils ftp gawk grep gettext git gzip hardlink hostname iftop info iproute ipset iputils jq kubernetes-cli less lua lvm2 make man nano net-tools nfs-utils nload nmap openssh-clients passwd procps-ng rsync sed sudo sysstat tar tcpdump tcping traceroute unzip util-linux vim wget which xz zip
+# less commonly used: sudo yum install -y mc
+echo
+
+echo
+echo IF NTPD IS INSTALLED, REMOVING IT. INSTALLING CHRONY AND A CONFIG FILE
+echo
+# We'll be using chrony for NTP, ntpd package may not even be installed
+# Default NTP in AWS works fine, but this is useful for other environments
+sudo yum remove -y ntp > /dev/null 2>&1
+sudo yum install -y chrony
+# Install chrony config file
+sudo cp chrony.conf /etc
+# No need to restart chrony since a reboot will be done
 echo
 
 echo
@@ -40,6 +79,7 @@ sudo cp install-kernel-headers.rhel.service /etc/systemd/system/install-kernel-h
 sudo systemctl daemon-reload
 sudo systemctl enable install-kernel-headers 
 # Not going to run it, because after reboot the new kernel will be loaded then this will be ran
+echo
 
 echo
 echo INSTALLING 6 HOUR SYSTEMD TIMER TO SHUTDOWN SYSTEM, TO LIMIT OUR CLOUD SPEND
@@ -78,7 +118,23 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF'
 sudo yum install -y kubectl
-# NOTE autocompletion is not setup. https://kubernetes.io/docs/tasks/tools/install-kubectl/#enabling-shell-autocompletion
+# autocompletion per https://kubernetes.io/docs/tasks/tools/install-kubectl/#enabling-shell-autocompletion
+sudo echo "source <(kubectl completion bash)" >> /etc/bashrc
+echo
+
+echo
+echo INSTALING .VIMRC FILE TO SET VI DEFAULTS
+echo
+#sudo mv ~/.vimrc ~/vimrc.old > /dev/null 2>&1
+sudo cp vimrc ~/.vimrc
+echo
+
+echo
+echo SETTIG TIMEZONE TO BE UTC
+echo
+# In AWS this is the default for CentOS, but since we might not be using AWS we'll do it here
+# Don't use this in a container
+sudo timedatectl set-timezone UTC
 echo
 
 #### PYTHON 3.6
@@ -86,7 +142,6 @@ echo
 ## note pip is already included python36-pip
 # Note python2 is still installed, and this does not symlink to python36
 # This doesn't work outside of a container, not sure why
-# Note pip is built into python in 3.4 and later
 #sudo python36 -m ensurepip --default-pip
 #pip3 -v
 #sudo pip3 install --upgrade pip
@@ -94,13 +149,13 @@ echo
 #sudo pip3 install virtualenv
 ####
 
-#### JAVA 1.8
-# 1.11 = java-11-openjdk-headless
-# sudo yum install -y java-1.8.0-openjdk-headless
+#### JAVA 1.11
+# 1.8 = java-1.8.0-openjdk-headless
+# sudo yum install -y java-11-openjdk-headless
 # java -version
 ## Verify this is the right version
-#ENV JAVA_VERSION 8u181
-## need to add env vars in app definition such as java_args
+#todo: dockerfile ENV JAVA_VERSION ????
+## FOR 1.8 need to add env vars in app definition such as java_args
 ## note, use: java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap
 ## https://dzone.com/articles/running-a-jvm-in-a-container-without-getting-kille
 ####
@@ -112,10 +167,17 @@ echo
 #sudo unzip terraform_0.11.11_linux_amd64.zip -d /usr/bin
 
 
-#### yum clean all
+#### sudo rm -rf /var/cache/yum
 echo
-echo DONE, NOW REBOOT AND RUN setup-linux2.sh TO COMPLETE THIS INSTALL
 echo
-
+echo DONE. THE FOLLOWING STEPS NEED TO BE PERFORMED:
+echo
+echo 1. Reboot. This will allow the kernel headers to be loaded by the service
+echo   that was added.
+echo 2. sudo rm -f /etc/machine-id
+echo 3. Shutdown
+echo 4. Take a snapshot.
+echo
+echo
 
 
